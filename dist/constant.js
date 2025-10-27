aeacf3c4dd8ee45dbe767ef3966e5fa79856fb06
LiDianQiu <39948545+LiDianQiu@users.noreply.github.com>
2025-10-27 14:51:18 +0800

    Add constant.js with radar ring configurations

---
+++ dist/constant.js
@@ -0,0 +1,17 @@
+var o = /* @__PURE__ */ ((s) => (s[s.Adopt = 0] = "Adopt", s[s.Trial = 1] = "Trial", s[s.Assess = 2] = "Assess", s[s.Hold = 3] = "Hold", s))(o || {});
+const e = {
+  0: "优选",
+  1: "可选",
+  2: "慎选",
+  3: "禁选"
+}, t = {
+  0: "#5ba300",
+  1: "#009eb0",
+  2: "#c7ba00",
+  3: "#e09b96"
+};
+export {
+  o as RadarRing,
+  t as radarRingColors,
+  e as radarRingNames
+};
